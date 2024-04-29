ec1245fcf9397ea1da26b6aa36a009928b365749
KeerthanaM16 <143389814+KeerthanaM16@users.noreply.github.com>
2024-04-29 19:02:08 +0530

    Add files via upload

---
+++ labfat.py
@@ -0,0 +1,14 @@
+
+def fib(n):
+
+    if n in (0, 1):
+        return n
+
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
+if __name__ == "__main__":
+    n = int(input("Enter the index of the Fibonacci number: "))
+    print(fib(n))
